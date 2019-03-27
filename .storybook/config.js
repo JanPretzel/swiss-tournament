385cc315ecfb3a90e1ddf275481bec93df618904
Jan Pretzel <jan.pretzel@gmail.com>
2019-03-27 21:41:36 +0100

    Update storybook config for styled-components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,14 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+
+import theme from '../src/theme';
 
 function loadStories() {
   require('./stories');
 }
 
+const withTheme = story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>;
+
+addDecorator(withTheme);
 configure(loadStories, module);
