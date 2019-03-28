fae42da4e4a1f8eb1603e93c6326fc8bb6d3737c
Jan Pretzel <jan.pretzel@gmail.com>
2019-03-28 21:58:13 +0100

    Updates

---
+++ precache-manifest.51fb93304eaac1bf4868d0ee682aeb0b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d422cc85ffd21a022c00",
+    "url": "/swiss-tournament/static/js/main.0dc0b4b3.chunk.js"
+  },
+  {
+    "revision": "a10a6cee5cec67dbddf4",
+    "url": "/swiss-tournament/static/js/runtime~main.4da2756a.js"
+  },
+  {
+    "revision": "4559b08b49e8c39ca88f",
+    "url": "/swiss-tournament/static/js/2.e3e6621d.chunk.js"
+  },
+  {
+    "revision": "dfd5103ece1de2e65230837efcf829b1",
+    "url": "/swiss-tournament/index.html"
+  }
+];
