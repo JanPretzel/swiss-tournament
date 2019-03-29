d37c49ace1b0be30f68ebbbcfef496f2a18eeeee
Jan Pretzel <jan.pretzel@gmail.com>
2019-03-29 23:39:04 +0100

    Disable dragging on team images

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -4,7 +4,7 @@ import { TeamImage } from './styles';
 import { Team as Props } from '../../types';
 
 const Team: React.FC<Props> = ({ logoUrl, teamName }) => {
-  return <TeamImage src={logoUrl} alt={teamName} />;
+  return <TeamImage draggable={false} src={logoUrl} alt={teamName} />;
 };
 
 export default Team;
